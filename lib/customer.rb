a786e7722a4081e394ee0efac385d807c5daf043
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-21 15:30:19 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,36 @@
+
+require 'pry'
 class Customer
-  
+
+  attr_accessor :name, :age
+ 
+  @@all = []
+ 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+ def new_meal(waiter, total, tip)
+   Meal.new(waiter, self, total, tip)
+ end
+ 
+ def meals
+    Meal.all.select {|meal| meal.customer == self}
+ end
+ 
+ def waiters
+   all_my_meals = Meal.all.select {|meal| meal.customer == self}
+   all_my_waiters = []
+   all_my_meals.each do |my_meal|
+     all_my_waiters << my_meal.waiter
+   end
+   all_my_waiters
+ end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,36 @@
 class Waiter
-
+ 
+  attr_accessor :name, :yrs_experience
+ 
+  @@all = []
+ 
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+ 
+ def new_meal(customer, total, tip)
+   Meal.new(self, customer, total, tip)
+ end
+ 
+ def meals
+    Meal.all.select {|meal| meal.waiter == self}
+ end
+ 
+ def best_tipper
+  
+   
+   
+   
+   
+   all_my_meals = meals
+   meals.reduce { |my_meal| my_meal.tip.max }
+ end
+ 
+  def self.all
+    @@all
+  end
+ 
+ 
 end
