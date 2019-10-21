d8fd01af34d0045d9d31b47834bf2eb7511b76c9
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-21 15:48:49 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -35,13 +35,4 @@ class Customer
     all_my_waiters
 end
 
-# def waiters
-#   all_my_meals = Meal.all.select {|meal| meal.customer == self}
-#   all_my_waiters = []
-#   all_my_meals.each do |my_meal|
-#     all_my_waiters << my_meal.waiter
-#   end
-#   all_my_waiters
-# end
-
 end
