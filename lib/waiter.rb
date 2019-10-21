cf48da93561cc1ddbd96a2c7544afe8b549a51b3
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-21 15:42:03 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -20,12 +20,14 @@ class Waiter
  
  def best_tipper
    biggest_tip = 0
+   biggest_tipper = NIL
    all_my_meals = Meal.all.select do |meal| 
       if meal.waiter == self && meal.tip >= biggest_tip
         biggest_tip = meal.tip
+        biggest_tipper  = meal.customer
       end
    end
-   biggest_tip
+   biggest_tipper
  end
  
   def self.all
